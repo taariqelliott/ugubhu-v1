295368076810e7d13e3fef47663c055ae78310cf
Taariq Elliott <codebytaariq@gmail.com>
2026-01-12 17:14:35 -0500

    Fixed hero package issue preventing window form loading player

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,10 +1,10 @@
 import { app, BrowserWindow, screen } from "electron";
 import { createRequire } from "node:module";
-import { fileURLToPath } from "node:url";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 createRequire(import.meta.url);
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-process.env.APP_ROOT = path.join(__dirname, "..");
+const __dirname$1 = path.dirname(fileURLToPath(import.meta.url));
+process.env.APP_ROOT = path.join(__dirname$1, "..");
 const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
 const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
@@ -16,7 +16,7 @@ function createWindow() {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname, "preload.mjs")
+      preload: path.join(__dirname$1, "preload.mjs")
     },
     width: 1e3,
     height: 750,
